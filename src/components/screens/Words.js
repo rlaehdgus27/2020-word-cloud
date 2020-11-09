a6e84de25d67d447a2a159edbaa1b8a5764ff2f2
rlaehdgus27 <rlaehdgus27@gmail.com>
2020-11-10 01:50:32 +0900

    firebase words add

--- src/components/screens/Words.js
+++ src/components/screens/Words.js
@@ -1,13 +1,56 @@
 import React, { Component } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+const databaseURL = "https://wordcloud-e549f.firebaseio.com/";
 
 class Words extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      words: null,
+    };
+  }
+
+  _get = () => {
+    fetch(`${databaseURL}/words.json`)
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error(res.statusText);
+        }
+
+        return res.json();
+      })
+      .then((response) => this.setState({ words: response }));
+  };
+
+  componentDidMount = () => {
+    this._get();
+  };
+
   render() {
+    const { words } = this.state;
+
     return (
-      <Card>
-        <CardContent>Words SCREEN!!</CardContent>
-      </Card>
+      <div>
+        {words &&
+          words.map((word, idx) => {
+            return (
+              <Card key={idx}>
+                <CardContent>
+                  <Typography color="textSecondary" gutterBottom>
+                    가중치: {word.weight}
+                  </Typography>
+                  <Typography variant="h5" component="h2">
+                    {word.word}
+                  </Typography>
+                </CardContent>
+              </Card>
+            );
+          })}
+      </div>
     );
   }
 }
